512a80f14e15d6929277dcbb1a64480d9ef884ed
Mica <muribez@unsa.edu.pe>
2026-09-24 00:44:27 -0500

    settings

--- config/settings.py
+++ config/settings.py
@@ -30,10 +30,6 @@ INSTALLED_APPS = [
     "apps.sanciones",
     "apps.auditoria",
     "apps.notificaciones",
-    "apps.sanciones",
-    "apps.perdidas",
-    "apps.reportes",
-    "apps.auditoria",
 ]
 
 MIDDLEWARE = [
